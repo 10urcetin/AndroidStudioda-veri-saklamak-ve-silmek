7196ad267716b111a60fe2f7ba618992df471b66
Onur Cetin <onucetin003@gmail.com>
2022-08-26 17:25:55 +0300

    Android Studioda veri saklama-silme
    Android Studioda 2 buton aracılığı ile veri saklama ve silme işlemi

---
+++ StoringData/app/src/main/java/com/onurcetin/storingdata/MainActivity.java
@@ -0,0 +1,61 @@
+package com.onurcetin.storingdata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText=findViewById(R.id.editTextNumber);
+        textView=findViewById(R.id.editText2);
+
+        sharedPreferences=this.getSharedPreferences("com.onurcetin.storingdata", Context.MODE_PRIVATE);
+        int storedAge=  sharedPreferences.getInt("storedAge",0);
+        textView.setText("Your age: "+storedAge);
+
+        if(storedAge==0){
+            textView.setText("Your age: ");
+
+        }else{
+            textView.setText("Your age: "+storedAge);
+        }
+    }
+    public void save(View view){
+        if (!editText.getText().toString().matches("")){
+            int userAge=Integer.parseInt(editText.getText().toString());
+            textView.setText("Your age: "+userAge);
+
+            sharedPreferences.edit().putInt("storedAge",userAge).apply();
+
+
+
+        }
+
+
+    }
+    public void delete(View view){
+        int storedData=sharedPreferences.getInt("storedAge",0);
+        if( storedData!=0){
+            sharedPreferences.edit().remove("storedAge");
+            textView.setText("Your age: ");
+        }
+
+
+    }
+}
